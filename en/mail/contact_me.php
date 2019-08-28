fb8821451725a96873a76be17432113a7deb5050
Keldibek23 <k.kurmangaliyev@gmail.com>
2019-08-28 20:40:33 +0500

    upload

---
+++ en/mail/contact_me.php
@@ -0,0 +1,28 @@
+
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "Введите данные!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'info@anti-office.kz'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Новое сообщение:  $name";
+$email_body = "Вы получили новое сообщение на сайте.\n\n"."Вот детали сообщения:\n\nИмя: $name\nEmail: $email_address\nТелефон: $phone\nСообщение:\n$message";
+$headers = "From: no-reply@anti-office.kz\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
+
